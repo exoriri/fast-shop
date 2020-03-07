d7784830e7c9961b537d46b6fb53788a94187505
Lavrik Roman <lavrikrom2@gmail.com>
2020-03-07 22:57:33 +0300

    feat(*): parcel build

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const url = process.env.NODE_ENV = 'developement' ? 'http://127.0.0.1:3000/' : 'https://afternoon-inlet-00968.herokuapp.com/';
+
 export const signup = (email, password) => (
     axios.post('/signup', {
         email,
